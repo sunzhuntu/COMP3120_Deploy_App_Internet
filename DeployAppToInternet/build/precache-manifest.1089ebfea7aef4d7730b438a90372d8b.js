eaafe2b51cf63c5aa446c1b55ba3112c71c5b1e4
arikman <708911895@qq.com>
2020-08-17 17:21:33 +0800

    deploy app to Internet

---
+++ DeployAppToInternet/build/precache-manifest.1089ebfea7aef4d7730b438a90372d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d16f27420d6ca93ffd4cee68573679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a5cc4df729da8a7ae69",
+    "url": "/static/css/main.2ce93fec.chunk.css"
+  },
+  {
+    "revision": "ce03545e6fdb3a60c0be",
+    "url": "/static/js/2.b66e5761.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b66e5761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5cc4df729da8a7ae69",
+    "url": "/static/js/main.1d17ce3a.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
